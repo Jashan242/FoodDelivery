2d8d28168d23273d3cf27d06270eeafba6d56c64
Jashandeep Kaur <deep242jashan@gmail.com>
2024-12-22 21:24:45 +0530

    changes in cart component

--- frontend/src/components/Checkout.jsx
+++ frontend/src/components/Checkout.jsx
@@ -113,7 +113,7 @@ const Checkout = () => {
             }
         } catch (error) {
             console.error("Error placing order:", error);
-            toast.error("Failed to place order");
+            toast.error("Failed to place order. Check all fields and try again.");
         }
     };
     
@@ -166,12 +166,12 @@ const Checkout = () => {
                                 required
                                 className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#b8165c]" 
                             />
-                            <button 
+                            {/* <button 
                                 type="submit"
                                 className="w-full bg-[#b8165c] font-bold text-white py-2 px-4 rounded-md hover:bg-white hover:text-[#b8165c] hover:border hover:border-[#b8165c]"
                             >
                                 Deliver to this address
-                            </button>
+                            </button> */}
                         </form>
                     </div>
 
